7651a12bc7eebffb005c9ce2322ecc34f9f415ae
PodlesniyIvanRUDN <podlessnaja101@gmail.com>
2025-01-28 02:33:39 +0300

    cleaning

--- past studies/React-studies/app_4(Login + Posts)/social-network/client/src/api/ValidateResponse.ts
+++
@@ -1,9 +0,0 @@
-export async function validateResponse(response:Response): Promise<Response>{
-    if(!response.ok){
-        alert(await response.text() );
-        throw new Error(await response.text())
-
-    }
-
-    return response;
-}
